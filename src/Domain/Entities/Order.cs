fc5a7ad8846a094b4e5c11574cf1b3bdb94a3210
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-06-17 14:56:34 +0300

    feat: add Domain project with entities

---
+++ src/Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+namespace Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string Status { get; set; } = "Pending";
+
+    public ICollection<OrderDetail> OrderDetails { get; set; } = [];
+}
---
+++ src/Domain/Entities/OrderSagaState.cs
@@ -0,0 +1,14 @@
+using MassTransit;
+
+namespace Domain.Entities;
+
+public class OrderSagaState : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+    public int Version { get; set; }
+    public string CurrentState { get; set; } = string.Empty;
+
+    public int OrderId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public decimal TotalAmount { get; set; }
+}
