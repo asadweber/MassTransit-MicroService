28790709d59683c2d49b85b100cdf96ac0e9c721
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-06-17 11:58:49 +0300

    AA

--- src/Contracts/Consumers/InventoryConsumer.cs
+++ src/Contracts/Consumers/InventoryConsumer.cs
@@ -12,6 +12,7 @@ public class InventoryConsumer(ILogger<InventoryConsumer> logger) : IConsumer<Ch
         var msg = context.Message;
         logger.LogInformation("Checking inventory for Order {OrderId}",msg.OrderId);
 
+
         // TODO: real inventory check logic
         var isAvailable = true;
         
