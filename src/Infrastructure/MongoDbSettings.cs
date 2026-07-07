2e40176aa07f2dcc656ead8f9bfa681d19ca229c
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-07-07 12:49:53 +0300

    AAA

--- src/Infrastructure/MongoDbSettings.cs
+++ src/Infrastructure/MongoDbSettings.cs
@@ -9,6 +9,5 @@ namespace Infrastructure
         public string ConnectionString { get; set; } = "mongodb://localhost:27017";
         public string DatabaseName { get; set; } = "OrderSagaDb";
         public string SagaCollection { get; set; } = "OrderSagas";
-        public string OutboxCollection { get; set; } = "OutboxMessages";
     }
 }
