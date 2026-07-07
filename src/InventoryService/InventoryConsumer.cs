e5730928c12a3a2086f28c55023cac2c9911c0d7
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-07-07 12:46:32 +0300

    AAA

--- src/InventoryService/InventoryConsumer.cs
+++ src/InventoryService/InventoryConsumer.cs
@@ -21,11 +21,11 @@ public class InventoryConsumer(ILogger<InventoryConsumer> logger, IOrderService 
         var isAvailable = true;
         foreach (var item in order.OrderDetails)
         {
-            if (item.ProductId == 1)
-            {
-                logger.LogWarning("Simulated failure for Order {OrderId}, redelivery count {RedeliveryCount}", msg.OrderId, context.GetRedeliveryCount());
-                throw new HttpRequestException("Inventory service unavailable");
-            }
+            //if (item.ProductId == 1)
+            //{
+            //    logger.LogWarning("Simulated failure for Order {OrderId}, redelivery count {RedeliveryCount}", msg.OrderId, context.GetRedeliveryCount());
+            //    throw new HttpRequestException("Inventory service unavailable");
+            //}
 
             var hasSufficientStock = await productService.HasSufficientStockAsync(item.ProductId, item.OrderQty);
             if (!hasSufficientStock)
