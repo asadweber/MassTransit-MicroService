63dfc1f90fa2aeb4eab0256173510f80cd14ffba
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-07-08 13:53:29 +0300

    Add

--- src/SagaDashboard/Program.cs
+++ src/SagaDashboard/Program.cs
@@ -72,7 +72,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "/{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseMassTransitDashboard();
 
