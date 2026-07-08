1b18d553fe971a246b15a76e1e2e04e832cdbbef
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-07-08 13:10:08 +0300

    OrderSagaDbOrderSagaDbOrderSagaDb

--- src/NotificationService/NotificationConsumer.cs
+++ src/NotificationService/NotificationConsumer.cs
@@ -1,3 +1,4 @@
+using Application.Interfaces;
 using Application.Messaging.Events;
 using MassTransit;
 using Microsoft.Extensions.Logging;
@@ -5,7 +6,7 @@ using Microsoft.Extensions.Logging;
 namespace NotificationService;
 
 [ExcludeFromConfigureEndpoints]
-public class NotificationConsumer(ILogger<NotificationConsumer> logger) : IConsumer<OrderConfirmed>
+public class NotificationConsumer(ILogger<NotificationConsumer> logger, IOrderService orderService) : IConsumer<OrderConfirmed>
 {
     public async Task Consume(ConsumeContext<OrderConfirmed> context)
     {
@@ -13,7 +14,9 @@ public class NotificationConsumer(ILogger<NotificationConsumer> logger) : IConsu
         using var _ = Serilog.Context.LogContext.PushProperty("CorrelationId", msg.CorrelationId);
         using var __ = Serilog.Context.LogContext.PushProperty("OrderId", msg.Order.Id);
 
-
+        var order = await orderService.GetByIdAsync(msg.Order.Id);
+        order.Status = "Complete";
+        await orderService.UpdateAsync(msg.Order.Id, order);
 
         logger.LogInformation("Confirmed. Notification sent.");
 
