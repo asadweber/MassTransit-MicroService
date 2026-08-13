ce34191105143a64e445c20e48ca0915041477fa
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-08-13 13:28:57 +0300

    AA

--- src/OrderSaga/Saga/OrderSagaDefinition.cs
+++ src/OrderSaga/Saga/OrderSagaDefinition.cs
@@ -78,7 +78,7 @@ namespace OrderSaga.Saga
 
             // Ensures messages for the same saga (CorrelationId) are processed in order,
             // even though ConcurrentMessageLimit allows multiple sagas in parallel.
-            //sagaConfigurator.UsePartitioner(endpointConfigurator.ConcurrentMessageLimit ?? 16, x => x.Saga.CorrelationId);
+            sagaConfigurator.UsePartitioner(endpointConfigurator.ConcurrentMessageLimit ?? 16, x => x.Saga.CorrelationId);
             
             // Same partitioner for all Order saga messages
             var partitioner =
