b9e3a604e27e84043e1b2d6084be175ff58c165b
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-08-16 16:05:57 +0300

    AA

--- src/Domain/Entities/OrderDetail.cs
+++ src/Domain/Entities/OrderDetail.cs
@@ -4,13 +4,13 @@ public class OrderDetail
 {
     public long Id { get; set; }
 
-    public long OrderId { get; set; }
-    public Order Order { get; set; } = null!;
-
     public long ProductId { get; set; }
     public Product Product { get; set; } = null!;
 
     public long OrderQty { get; set; }
     public decimal UnitPrice { get; set; }
     public decimal Total { get; set; }
+
+    public long OrderId { get; set; }
+    public Order Order { get; set; } = null!;
 }
